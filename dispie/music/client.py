22a48afc4c1d1af8b46584d4072e2d570c7ef45d
Pranoy Majumdar <kumarricky252@gmail.com>
2023-03-07 03:27:05 +0000

    init

--- dispie/music/client.py
+++ dispie/music/client.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
-from typing import Literal
+from typing import Literal, Optional
 
-from discord import Client, Embed, Message
-from dispie.music import Node, MusicPlayer
+from discord import Embed, Message, TextChannel
+from discord.ext.commands import Bot
+from .types import Node
 from pomice import NodePool, Track
+from logging import getLogger
 
+log = getLogger("Dispie Music Client")
 
 # https://github.com/freyacodes/Lavalink/releases/download/3.7.5/Lavalink.jar
 
@@ -14,13 +17,13 @@ class MusicClient:
     A class representing a client for a music player bot.
 
     Parameters:
-        - bot: The Discord bot client object.
-        - nodes: A list of node objects or a single node object to connect to.
+    - bot: The Discord bot client object.
+    - nodes: A list of node objects or a single node object to connect to.
     """
 
-    def __init__(self, bot: Client, nodes: list[Node] | Node) -> None:
+    def __init__(self, bot: Bot, nodes: list[Node] | Node) -> None:
         self.bot = bot
-        self.nodes = [nodes] if isinstance(nodes, list) else nodes
+        self.nodes = [nodes] if isinstance(nodes, dict) else nodes
         self._node_pool = NodePool()
         self._no_playing_embed = Embed(title="No track playing")
         self._playing_embed = Embed(title="Now playing")
@@ -56,7 +59,7 @@ class MusicClient:
         Sets the playing embed.
 
         Parameters:
-            - value: The new playing embed.
+        - value: The new playing embed.
         """
         self._playing_embed = value
 
@@ -73,7 +76,7 @@ class MusicClient:
         Sets the paused embed.
 
         Parameters:
-            - value: The new paused embed.
+        - value: The new paused embed.
         """
         self._paused_embed = value
 
@@ -90,7 +93,7 @@ class MusicClient:
         Sets the node pool object.
 
         Parameters:
-            - value: The new node pool object.
+        - value: The new node pool object.
         """
         self._node_pool = value
 
@@ -100,24 +103,50 @@ class MusicClient:
         Get the embed for the player.
         
         Parameters:
-            - player_type: Type of the player.
+        - player_type: Type of the player.
         """
         # TODO: Return later
-    async def handle_on_message(self, message: Message) -> None:
+
+
+    def get_message(self, message_id: int, channel_id: int) -> Optional[Message]:
+        if (mable := self.bot.get_partial_messageable(channel_id)):
+            if (message := mable.get_partial_message(message_id)):
+                return Message
+
+
+    async def setup_player_embed(self, channel: TextChannel) -> Message:
+        """
+        Setup the player embed.
+        Parameters:
+        - channel: The channel you want to setup the player. 
+        """
+        return await channel.send(
+            embed=self.get_player_embed(player_type="no_playing")
+        )
+
+    async def handle_on_message(self, message: Message, player_message_id: int) -> None:
         """
         A coroutine that handles the message event.
 
         Parameters:
-            - message: The message object.
+        - message: The message object.
+        - player_message_id: The message id of the player embed.
         """
-        # TODO: Implement message handling logic
+        if message.author.bot:
+            if message.author.id == self.bot.user.id:
+                await message.delete(delay=5)
+            await message.delete()
+            return
+        await self.bot.process_commands(message)
+        
+
 
     async def handle_track_start(self, track: Track) -> None:
         """
         A coroutine that handles the start of a new track.
 
         Parameters:
-            - track: The new track object.
+        - track: The new track object.
         """
         # TODO: Implement track start handling logic
 
@@ -126,17 +155,17 @@ class MusicClient:
         A coroutine that handles the end of a track.
 
         Parameters:
-            - track: The ending track object.
+        - track: The ending track object.
         """
         # TODO: Implement track end handling logic
 
     async def start_nodes(self):
         for nodes in self.nodes:
-            await self._node_pool.create_node(
+            node = await self._node_pool.create_node(
                 bot=self.bot,
                 host=nodes.get("host"),
                 port=nodes.get("port"),
                 password=nodes.get("password"),
                 identifier=nodes.get("identifier")
             )
-            
+            log.info(f"{node._identifier} has been created.")
--- main.py
+++ main.py
@@ -2,23 +2,53 @@ from discord.ext import commands
 from dispie.music import MusicClient, Node
 from config import token
 from pomice import Track
+from rich.logging import Handler, RichHandler, Highlighter
 import discord
 
-bot = commands.Bot(command_prefix="", intents=discord.Intents.all())
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 music = MusicClient(
     bot=bot,
-    nodes={
+    nodes=
+        {
         "host": "127.0.0.1",
-        "port"=3030,
-        "password"="youshallnotpass",
-        "identifier"="MAIN"
+        "port": 3030,
+        "password":"youshallnotpass",
+        "identifier":"MAIN"
     }
 )
 
+
+discord.utils.setup_logging(
+    handler=RichHandler(
+        show_time=False
+    )
+)
+
+database = {
+    "channel_id": ...,
+    "message_id": ...
+}
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.id == database['message_id'] and message.channel.id == database['channel_id']:
+        await music.handle_on_message(message, database['message_id'])
+
 @bot.event
 async def on_ready():
-    print("Logged in")
+    await music.start_nodes()
 
-    
-bot.run(token)
+@bot.command()
+async def setup(ctx: commands.Context, channel: discord.TextChannel):
+    msg = await music.setup_player_embed(channel)
+    print(msg.id, channel.id)
+
+async def main():
+    await bot.start(token)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
